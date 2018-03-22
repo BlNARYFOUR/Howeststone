9e11ff53965491633ae1c1204bcb1063d19d895d
kurt.sys <kurt.sys@howest.be>
2018-03-22 12:11:57 +0100

    setup base ci for Java/gradle

---
+++ server/src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test 
+    public void testSomeAppMethod() {
+        assertTrue("is this 'true'?", false);
+    }
+}
