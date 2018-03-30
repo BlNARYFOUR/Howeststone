80cf3c74365bf717b1fe0094be66ed5e12dc72e6
Lambert Brend <brend.lambert@student.howest.be>
2018-03-30 15:06:03 +0200

    Added js overlapping of cards
    Uneven amounts are still buggy

--- web_frontend/assets/js/gameBoard.js
+++ web_frontend/assets/js/gameBoard.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     setBackground();
+    overLapCards("you");
 
     /*
     document.getElementById("spark").addEventListener("click", burnFuse);
@@ -17,8 +18,33 @@ function burnFuse(e) {
 }
 */
 
-function overLapCards() {
+function overLapCards(parentClass) {
+    let parent = document.getElementsByClassName(parentClass);
+    let cards = document.querySelectorAll(`.${parentClass} .cards li`);
+    let totalFanWidth = 35;     // value in vh
+    let cardWidth = 11;         // value in vh
+    let diff = cardWidth * cards.length - totalFanWidth;
 
+    if(diff < 0)
+        diff = 0;
+
+    let offset = diff / (cards.length-1);
+
+    console.log(offset);
+
+    let minIndex = 0;
+    let halfAmount =  Math.floor(cards.length/2);
+    let firstHalfIndex = halfAmount-1;
+    let amountOfCards = cards.length;
+    let secondHalfIndex = amountOfCards-(halfAmount);
+
+    for(let i = minIndex; i < halfAmount; i++) {
+        cards[i].style.transform = "translateX(" + (offset*(firstHalfIndex-i) + offset/2) + "vh)";
+    }
+
+    for(let i = secondHalfIndex; i < amountOfCards; i++) {
+        cards[i].style.transform = "translateX(-" + (offset*(i-halfAmount) + offset/2) + "vh)";
+    }
 }
 
 function setBackground() {
