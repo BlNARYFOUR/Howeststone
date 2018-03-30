53a5f621e9ec9fcb4b91e5730abca492305a19c7
Lambert Brend <brend.lambert@student.howest.be>
2018-03-30 12:29:12 +0200

    Adding a js helper for layout

--- web_frontend/assets/js/gameBoard.js
+++ web_frontend/assets/js/gameBoard.js
@@ -3,20 +3,29 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     setBackground();
+
+    /*
     document.getElementById("spark").addEventListener("click", burnFuse);
     document.getElementById("fuse").addEventListener("click", burnFuse);
+    */
 }
 
+/*
 function burnFuse(e) {
     document.getElementById("spark").classList.add("burn");
     document.getElementById("fuse").classList.add("burn");
+}
+*/
+
+function overLapCards() {
+
 }
 
 function setBackground() {
     let rand = getRandomInt(2) + 1;
 
-    document.getElementById("playField").style.background = "no-repeat black url(assets/media/board_0" + rand + ".jpg) center center";
-    document.getElementById("playField").style.backgroundSize = "contain";
+    document.getElementById("gameBoard").style.background = "no-repeat black url(assets/media/board_0" + rand + ".jpg) center center";
+    document.getElementById("gameBoard").style.backgroundSize = "177.77vh 100vh";
 }
 
 function getRandomInt(max) {
