42e5928643420d276a2e359a0027d3cb704c7454
Mattijs Step <artuur80@outlook.com>
2018-05-08 10:25:56 +0200

    recoding the deckbuilder
    see gitlab for the discussion

--- web_frontend/assets/js/deckbuilder.js
+++ web_frontend/assets/js/deckbuilder.js
@@ -1,14 +1,13 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', init);
-let tutorialli = 1;
-let dragged;
+let tutorialLi = 1;
 
 function nextTutorial() {
-    document.querySelector('#tutorials li:nth-child(' +tutorialli+ ')').className = "hidden";
-    tutorialli += 1;
-    document.querySelector('#tutorials li:nth-child(' + tutorialli + ')').className = "";
-    if (tutorialli > 11){
+    document.querySelector('#tutorials li:nth-child(' +tutorialLi+ ')').className = "hidden";
+    tutorialLi += 1;
+    document.querySelector('#tutorials li:nth-child(' + tutorialLi + ')').className = "";
+    if (tutorialLi > 11){
         document.getElementById('next').removeEventListener('click', nextTutorial);
         document.querySelector('#tutorials').className = "hidden";
     }
@@ -24,20 +23,16 @@ function searchTest(e) {
     e.preventDefault();
     console.log(document.getElementById('search').value);
 
-    // Declare variables
-    let input, filter, ul, li, img, i;
-    input = document.getElementById('search');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("cards");
-    li =ul.getElementsByTagName('li');
-
+    // Declare variable
+    let cardsInDeck = document.querySelectorAll('#cards li');
+    // TODO fetch for search
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        img = li[i].getElementsByTagName("img")[0];
-        if (img.getAttribute('id').toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
+    for (let i = 0; i < cardsInDeck.length; i++) {
+        let img = cardsInDeck[i].getElementsByTagName("img")[0];
+        if (img.getAttribute('id').toUpperCase().indexOf(document.getElementById('search').value.toUpperCase()) > -1) {
+            cardsInDeck[i].style.display = "";
         } else {
-            li[i].style.display = "none";
+            cardsInDeck[i].style.display = "none";
         }
     }
 }
@@ -46,33 +41,91 @@ function donNotSubmit(e) {
     e.preventDefault();
 }
 
+function checkIfCardIsGoingToBeAdded() {
+    let rectDrag = dragSrcElement.getBoundingClientRect();
+    let deckRect = document.querySelector("#deck").getBoundingClientRect();
+    if ((rectDrag.right < deckRect.right) && (rectDrag.left > deckRect.left) && (rectDrag.bottom < deckRect.bottom+62) && (rectDrag.top > deckRect.top-103)){
+        addCardToDeck(itemThatIsBeingMoved);
+    }
+    try{
+        dragSrcElement.parentElement.removeChild(dragSrcElement);
+    } catch (err){
+        console.log('nothing can be removed' + err);
+    }
+    document.removeEventListener("touchmove", movingOfDragElement, false);
+    document.removeEventListener("touchend", checkIfCardIsGoingToBeAdded, false);
+    document.removeEventListener("mousemove", movingOfDragElement, false);
+    document.removeEventListener("mouseup", checkIfCardIsGoingToBeAdded, false);
+
+}
+function checkIfCardIsGoingToBeRemoved() {
+    try{
+        dragSrcElement.parentElement.removeChild(dragSrcElement);
+    } catch (err){
+        console.log('nothing can be removed' + err);
+    }
+    document.removeEventListener("touchmove", movingOfDragElement, false);
+    document.removeEventListener("touchend", checkIfCardIsGoingToBeRemoved, false);
+    document.removeEventListener("mousemove", movingOfDragElement, false);
+    document.removeEventListener("mouseup", checkIfCardIsGoingToBeRemoved, false);
+    removeCardFromDeck(itemThatIsBeingMoved);
+}
+
+function clickOnCardInCards(e) {
+    // TODO fetch
+    itemThatIsBeingMoved = e.target;
+    dragOffsetX = e.offsetX;
+    dragOffsetY = e.offsetY;
+    moved = false;
+    dragSrcElement = e.target.cloneNode(true);
+    document.body.appendChild(dragSrcElement);
+    dragSrcElement.removeAttribute('style');
+    movingOfDragElement(e);
+    dragSrcElement.style.zIndex = '1';
+    dragSrcElement.style.width = '23vh';
+    dragSrcElement.style.height= '34.83713356vh';
+    dragSrcElement.style.position = 'absolute';
+    dragSrcElement.style.background = e.target.style.background;
+    document.removeEventListener("mousedown", clickOnCardInCards);
+    document.removeEventListener("touchstart", clickOnCardInCards);
+    document.addEventListener("touchmove", movingOfDragElement, false);
+    document.addEventListener("mousemove", movingOfDragElement, false);
+    document.addEventListener("mouseup", checkIfCardIsGoingToBeAdded, false);
+    document.addEventListener("touchend", checkIfCardIsGoingToBeAdded, false);
+}
+
+function clickOnCardInDeck(e) {
+    // TODO fetch
+    itemThatIsBeingMoved = e.target;
+    dragOffsetX = e.offsetX;
+    dragOffsetY = e.offsetY;
+    moved = false;
+    dragSrcElement = e.target.cloneNode(true);
+    document.body.appendChild(dragSrcElement);
+    dragSrcElement.removeAttribute('style');
+    movingOfDragElement(e);
+    dragSrcElement.style.zIndex = '1';
+    dragSrcElement.style.width = '5vh';
+    dragSrcElement.style.height= '7.573289904vh';
+    dragSrcElement.style.position = 'absolute';
+    dragSrcElement.style.background = e.target.style.background;
+    document.removeEventListener("mousedown", clickOnCardInCards);
+    document.removeEventListener("touchstart", clickOnCardInCards);
+    document.addEventListener("touchmove", movingOfDragElement, false);
+    document.addEventListener("mousemove", movingOfDragElement, false);
+    document.addEventListener("mouseup", checkIfCardIsGoingToBeRemoved, false);
+    document.addEventListener("touchend", checkIfCardIsGoingToBeRemoved, false);
+}
+
 function init() {
-    /*tutorial();*/
+    // tutorial();
     document.querySelector('#deckbuilder aside form').addEventListener('submit', donNotSubmit);
     document.getElementById('search').addEventListener('input', searchTest);
-    document.querySelector('#firstadd').addEventListener('click', firstadd);
+    document.querySelector('#firstAdd').addEventListener('click', firstAdd);
 
-    checkallcards();
-    let myCards =MOCKMYCARDS() ;
+    let myCards = MOCKMYCARDS() ;
     MockCardsToDeckBuilder(myCards);
-
-    let cardInDeck = document.querySelectorAll('#cards li');
-    for (let i = 0; i < cardInDeck.length; i++){
-        cardInDeck[i].addEventListener('dragstart', function () {
-            dragged = this.innerHTML;
-        });
-    }
-    document.querySelector('#deck').addEventListener('dragover', function(e) { e.preventDefault();});
-    document.querySelector('#deck').addEventListener('drop', function () {
-        if ((this.getAttribute('id') === 'deck')){
-            try{
-                if (dragged.indexOf('draggable') !== -1){
-                    addCardToDeck(dragged);
-                    dragged = null;
-                }
-            }catch(error){}
-        }
-    });
+    checkAllCards();
 
     let inputs = document.querySelectorAll('#secondFilter input');
     for (let i = 0 ; i < inputs.length; i++){
@@ -101,41 +154,28 @@ function init() {
 }
 
 function MockCardsToDeckBuilder(cards) {
-
+    // TODO fetch
     for (let i = 0; i < cards.length; i++) {
         let imgUrl = cards[i]["img"];
         let imgID = cards[i]["name"];
-        //console.log(imgID);
-        document.getElementById('cards').innerHTML += '<li class ="cardInDeck"><figure draggable=true><img src="'+imgUrl+'" alt="'+imgID+'" title="'+imgID+'" id="'+imgID+'">'+'</figure></li>';
+
+        document.getElementById('cards').innerHTML += '<li class ="cardInDeck"><figure><img src="'+imgUrl+'" alt="'+imgID+'" title="'+imgID+'" id="'+imgID+'">'+'</figure></li>';
     }
 }
 
 
-
-
-
-
-function checkallcards() {
-    let cardInDeck = document.querySelectorAll(".cardInDeck");
-    for(let i = 0 ; i < cardInDeck.length; i++){
-        cardInDeck[i].addEventListener('dblclick', addCardToDeck);
-        cardInDeck[i].addEventListener('dragstart', )
+function checkAllCards() {
+    let cardsInDeck = document.querySelectorAll('#cards li');
+    for (let i = 0; i < cardsInDeck.length; i++){
+        cardsInDeck[i].addEventListener("mousedown", clickOnCardInCards);
+        cardsInDeck[i].addEventListener("touchstart", clickOnCardInCards);
+        cardsInDeck[i].addEventListener('dblclick', addCardToDeckDblClick);
     }
     let chosenCards = document.querySelectorAll(".chosenCards");
-    let lenghtAllCards = document.querySelectorAll(".two").length + chosenCards.length;
-    document.getElementById('cardAmount').innerHTML = lenghtAllCards + '/30';
-    document.querySelector('#cards').addEventListener('dragover', function( event ) { event.preventDefault();});
-    document.querySelector('#cards').addEventListener('drop', function () {
-        if ((this.getAttribute('id') === 'cards') && (dragged !== null)){
-            removeCardFromDeck(dragged);
-            dragged = null;
-        }
-    });
-    for(let i = 0 ; i < chosenCards.length; i++){
-        chosenCards[i].addEventListener('dblclick', removeCardFromDeck);
-        chosenCards[i].addEventListener('dragstart', function () {
-            dragged = this
-        });
+    let lengthAllCards = document.querySelectorAll(".two").length + chosenCards.length;
+    document.getElementById('cardAmount').innerHTML = lengthAllCards + '/30';
+    for (let i = 0; i < chosenCards.length; i++){
+        chosenCards[i].addEventListener('dblclick', removeCardFromDeckDblClick);
     }
 }
 
@@ -145,79 +185,64 @@ function unselectFilter(e) {
 }
 function disableFilter(e) {
     e.preventDefault();
-    e.target.checked = e.target.checked !== true;
 }
-function firstadd() {
+function firstAdd() {
     let chosenCards = document.querySelectorAll(".chosenCards");
     if (chosenCards.length === 30){
         document.querySelector('.save').innerHTML = 'full deck';
     }
-    /* apart scherm dat vraagt of deck moet opgeslaan worden*/
     document.querySelector('.main').classList.toggle('hidden');
     document.querySelector('.save').classList.toggle('hidden');
 }
 
-
-function addCardToDeck(e) {
-    let dit;
-    try {
-        dit = this.innerHTML;
-    }catch(error) {
-        dit = e;
-    }
-    console.log(dit);
-    console.log('hier zit error in vind het aub');
+function addCardToDeckDblClick(e) {
+    addCardToDeck(e.target);
+    console.log('does this work?')
+}
+function addCardToDeck(card) {
     let chosenCards = document.querySelectorAll(".chosenCards");
+    let images = document.querySelectorAll(".chosenCards img");
     let sameCard = 0;
-    for(let i = 0; i < chosenCards.length; i++){
-        if (dit === chosenCards[i].innerHTML){
+    for(let i = 0; i < images.length; i++){
+        if (card.getAttribute('id') === images[i].getAttribute('id')){
             sameCard += 1;
         }
     }
-
     if (chosenCards.length >= 30){
         console.error('to much cards');
     }
     else {
         if (sameCard === 0){
-            document.getElementById("deck").innerHTML += "<li class='chosenCards'>"+dit+"</li>";
-            checkallcards();
+            let cloneCard = card.cloneNode(true);
+            let cardHolder = document.createElement("li");
+            cardHolder.classList.add('chosenCards');
+            document.getElementById("deck").appendChild(cardHolder).appendChild(cloneCard);
+            checkAllCards();
         } else {
-            let cardType = "normal";
+            let cardType = 'normal';
             if (sameCard === 1 && cardType !== "legendary"){
-                for(let i = 0; i < chosenCards.length; i++){
-                    if (dit === chosenCards[i].innerHTML){
-                        chosenCards[i].classList.add("two");
+                for(let i = 0; i < images.length; i++){
+                    if (card.getAttribute('id') === images[i].getAttribute('id')){
+                        images[i].classList.add("two");
                     }
                 }
                 // change the old one
-                checkallcards();
+                checkAllCards();
             } else {
                 console.error('cannot add more');
             }
         }
     }
 }
+function removeCardFromDeckDblClick(e) {
+    removeCardFromDeck(e.target)
+}
 
-function removeCardFromDeck(e) { //remove eventlistener niet vergeten (nu nog zonder)
-    let dit;
-    try {
-        let nouse = this.classList;
-        dit = this;
-    }catch(error) {
-        dit = e;
-    }
-    if (dit.classList.contains('two')){
-        dit.classList.remove('two');
+function removeCardFromDeck(card) { //remove eventlistener niet vergeten (nu nog zonder)
+    if (card.classList.contains('two')){
+        card.classList.remove('two');
     } else {
-        dit.parentNode.removeChild(dit);
+        card.parentNode.remove();
     }
-    checkallcards();
-}
-function nodetailOfCard(e){
-    this.lastChild.lastChild.style.display = 'none';
-}
-
-function detailOfCard(e) {
-    this.lastChild.lastChild.style.display = 'block';
+    checkAllCards();
 }
