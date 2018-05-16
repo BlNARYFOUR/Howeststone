f48dde4b7e294136da1bb75dac9218dbfcfd0946
Euphoninator <bert.1999@hotmail.be>
2018-05-16 12:00:33 +0200

    Added a Method constructors heroPower

--- java_backend/src/main/java/hero/HeroPower.java
+++ java_backend/src/main/java/hero/HeroPower.java
@@ -6,7 +6,21 @@ public class HeroPower {
     private int manaCost;
     private int attack;
 
+    /*public HeroPower(int heroPowerId, String heroPowerName, int manaCost, int attack) {
+        this.heroPowerId = heroPowerId;
+        this.heroPowerName = heroPowerName;
+        this.manaCost = manaCost;
+        this.attack = attack;
+    }
+
+    public HeroPower(int heroPowerId, String heroPowerName, int manaCost) {
+        this(heroPowerId, heroPowerName, manaCost, 0);
+    }*/
+
     public void executeHeroPower(int heroPowerId) {
-        
+        // check For Two Mana;
+        // heroIdPower  = Paladin ==> check field is full ==> add 1/1 minion to field
+        // heroIdPower = Mage ==> frontend activeren om target te selecteren ==> levens target in backend -1
+        // check if dead here? or with function of lives decrease
     }
 }
