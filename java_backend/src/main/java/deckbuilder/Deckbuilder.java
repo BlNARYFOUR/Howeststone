b2ccc574138a0d6987cc0007ca169db7bf230320
Euphoninator <bert.1999@hotmail.be>
2018-05-16 16:25:54 +0200

    added essential functions to Deckbuilder
    more precisely empty functions

--- java_backend/src/main/java/deckbuilder/Deckbuilder.java
+++ java_backend/src/main/java/deckbuilder/Deckbuilder.java
@@ -3,6 +3,7 @@ package deckbuilder; /*
   Have a nice day!
  */
 
+import com.sun.xml.internal.bind.v2.TODO;
 import hero.Hero;
 
 public class Deckbuilder {
@@ -13,13 +14,49 @@ public class Deckbuilder {
     private String sortOption;
 
 
-    public void saveDeck() {
-        //checkIfDeckIsFull()
-        // INSERT_DECK(deckName, getSelectedHero.getId)
-        // INSERT_CARDSINDECK
+    public void addCardToDeck() {
+
     }
 
+    public void deleteCardToDeck() {
 
+    }
+
+    public void saveDeck() {
+        // checkIfDeckIsFull()
+        // INSERT_DECK(deckName, getSelectedHero.getId)
+        // INSERT_CARDSINDECK
+        //TODO
+    }
+
+    public void showCardsOfHero(Hero selectedHero) {
+        //TODO
+    }
+
+    public void filterCards() {
+        //TODO
+    }
+
+    public void sortCards() {
+        //TODO
+    }
+
+    public void searchCards() {
+        //TODO
+    }
+
+    public void switchDeck() {
+        if (deckIsSaved()) {
+            //TODO
+        } else {
+            throw new IllegalArgumentException("Your current deck hasn't been saved yet!");
+        }
+    }
+
+    private boolean deckIsSaved() {
+        return true;
+        //TODO
+    }
 
     public Hero getSelectedHero() {
         return selectedHero;
