08848f8bbbaa0ef7f52c0a39877a98d06c9df944
Euphoninator <bert.1999@hotmail.be>
2018-05-16 11:49:45 +0200

    Heropower class added
    fields+empty method execute

---
+++ java_backend/src/main/java/hero/HeroPower.java
@@ -0,0 +1,12 @@
+package hero;
+
+public class HeroPower {
+    private int heroPowerId;
+    private String heroPowerName;
+    private int manaCost;
+    private int attack;
+
+    public void executeHeroPower(int heroPowerId) {
+        
+    }
+}
