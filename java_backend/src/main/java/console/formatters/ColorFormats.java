bb49d7896598ebbd2a31dbf8358bd305dbdd4836
Lambert Brend <brend.lambert@student.howest.be>
2018-05-17 16:24:53 +0200

    Added a summary to ColorFormats

--- java_backend/src/main/java/console/formatters/ColorFormats.java
+++ java_backend/src/main/java/console/formatters/ColorFormats.java
@@ -1,6 +1,8 @@
 package console.formatters;
 
 public class ColorFormats {
+    // \033 means "format the next" number after [ means what to format and m is the end of the format.
+    // look at following link for more info -> TODO: http://www.santhoshreddymandadi.com/java/coloring-java-output-on-console.html
     private static final String RESET = "\033[0m";
     private static final String RED = "\033[31m";
     private static final String GREEN = "\033[32m";
