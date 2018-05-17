6228a4c758b35e99df366ec33f70b9020fd47202
Lambert Brend <brend.lambert@student.howest.be>
2018-05-17 15:45:46 +0200

    Console code adjusted, hero selection added

--- java_backend/src/main/java/console/StartUp.java
+++ java_backend/src/main/java/console/StartUp.java
@@ -1,20 +1,69 @@
 package console;
 
 import game.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class StartUp {
+    private Scanner scanner = new Scanner( System.in );
+
     public static void main(String[] args) {
         new StartUp().run();
     }
 
     private void run() {
         Game howeststone = new Game();
-        System.out.println(howeststone.getHeroes());
-        howeststone.setHero("Paladin");
-        System.out.println(howeststone.getDecks());
+        final List<String> HEROES = new ArrayList<>(Arrays.asList(howeststone.getHeroNames()));
+
+        System.out.println("Select one of the following heroes:");
+        System.out.println(formatList(HEROES));
+        String selectedHero = askInputUntilFoundInList(HEROES);
+        howeststone.setHero(selectedHero);
+
+        System.out.println(howeststone.getDeckNames());
         // TODO standard (random) deck ???
         // not yet full
         // howeststone.setDeck("Standard");
         // System.out.println(howeststone.getDeck());
+    }
+
+    private String askInputUntilFoundInList(List<String> list) {
+        String input;
+
+        do {
+            System.out.print("Input here: ");
+            input = scanner.nextLine();
+
+            if(!list.contains(input)) {
+                System.out.println("\033[31;1mThis is not an option!");
+                System.out.println("\033[0mThe available options are:");
+
+                System.out.println(formatList(list));
+            }
+        }
+        while(!list.contains(input));
+
+        System.out.println("\033[31;1mSelected: \033[32m" + input + "\033[0m");
+
+        return input;
+    };
+
+    @NotNull
+    private String formatList( List<String> list) {
+        StringBuilder strBuilder = new StringBuilder();
+
+        for(String str : list) {
+            strBuilder.append("\t- ");
+            strBuilder.append(str);
+            strBuilder.append('\n');
+        }
+
+        return  strBuilder.toString();
     }
 }
--- java_backend/src/main/java/game/Game.java
+++ java_backend/src/main/java/game/Game.java
@@ -4,7 +4,14 @@ import cards.CardCollection;
 import hero.Hero;
 import sun.security.krb5.internal.crypto.DesMacCksumType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    // TODO: MOCKERS
+    protected static final String[] MOCKED_HEROES = {"Mage", "Paladin"};
+
+    // TODO: actual fields
     private Hero you;
     private CardCollection deck;
 
@@ -17,8 +24,8 @@ public class Game {
     }
 
     // TODO change to multiple Heroes
-    public String getHeroes() {
-        return "Mage Paladin";
+    public String[] getHeroNames() {
+        return MOCKED_HEROES;
     }
 
     // TODO change to multiple Decks
@@ -35,5 +42,15 @@ public class Game {
 
     public CardCollection getDeck() {
         return deck;
+    }
+
+    public List<String> getDeckNames() {
+        List<String> mocked = new ArrayList<String>();
+
+        mocked.add("Standard");
+        mocked.add("Deck 1");
+        mocked.add("Deck 2");
+
+        return mocked;
     }
 }
