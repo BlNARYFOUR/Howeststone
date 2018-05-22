5867bd3334bd7f241c155311f7db1487c407f84f
Lambert Brend <brend.lambert@student.howest.be>
2018-05-22 10:16:11 +0200

    MAde private public

--- java_backend/src/main/java/api/Server.java
+++ java_backend/src/main/java/api/Server.java
@@ -14,7 +14,7 @@ public class Server {
         this.port = port;
     }
 
-    private void start() {
+    public void start() {
         final Javalin server = Javalin.create()
                 .port(port)
                 .enableStaticFiles("web")
