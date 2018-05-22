77238a30cec431feb2c53b6e0e68e8610ec90ea2
Euphoninator <bert.1999@hotmail.be>
2018-05-22 14:00:38 +0200

    fixed DoThingsWithDatabase

---
+++ java_backend/src/main/java/be/howest/ti/threebeesandme/howeststone/db/DoThingsWithDatabase.java
@@ -0,0 +1,48 @@
+package be.howest.ti.threebeesandme.howeststone.db; /*
+  Created by Bert on 22/05/2018.
+  Have a nice day!
+ */
+
+import java.sql.*;
+
+public class DoThingsWithDatabase {
+    public static void main(String[] args) {
+        new be.howest.ti.threebeesandme.howeststone.db.DoThingsWithDatabase().run();
+    }
+
+    SqlDatabase db;
+
+    private void run() {
+        db = new SqlDatabase("localhost:88", "root", "");
+    }
+
+    private void insertDeck(String deckName, int heroId) {
+        try (
+                Connection conn = db.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(SqlStatements.INSERT_DECK,
+                        Statement.RETURN_GENERATED_KEYS);
+            ) {
+                stmt.setString(1, deckName);
+                stmt.setInt(2, heroId);
+                final int AFFECTED_ROWS = stmt.executeUpdate();
+
+                if (AFFECTED_ROWS == 0) {
+                    throw new SQLException("No deck created: no rows affected.");
+                }
+
+                try (ResultSet rs = stmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        final long ID = rs.getLong(1);
+                        System.out.printf("%s now has ID %d", deckName, ID);
+                    } else {
+                        throw new SQLException("No deck created: no ID obtained.");
+                    }
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
