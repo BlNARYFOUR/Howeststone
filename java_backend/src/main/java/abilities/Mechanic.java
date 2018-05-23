10ad5ccc417acc48ec0aa9500e4160b8830bd141
Vermeulen Bram <bram.vermeulen@student.howest.be>
2018-05-23 11:56:57 +0200

    Some updates to the mechanic and ability class
    nu gaan eten nom nom nom

--- java_backend/src/main/java/abilities/Mechanic.java
+++ java_backend/src/main/java/abilities/Mechanic.java
@@ -1,5 +1,19 @@
 package abilities;
 
-public class Mechanic {
+import cards.Card;
+
+public abstract class Mechanic {
+    private final Mechanics MECHANIC_TYPE;
+
+    public Mechanic(Mechanics MECHANIC_TYPE) {
+        this.MECHANIC_TYPE = MECHANIC_TYPE;
+    }
+
+    public Mechanics getMECHANIC_TYPE() {
+        return MECHANIC_TYPE;
+    }
+
+    public abstract boolean executeAbility(Card self, Card target);
+}
 
 }
