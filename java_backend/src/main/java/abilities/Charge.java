fd1f30f6db80ee8ea0f57ae21489beeef55bb57c
Euphoninator <bert.1999@hotmail.be>
2018-05-23 17:04:41 +0200

    made some little changes

--- java_backend/src/main/java/abilities/Charge.java
+++ java_backend/src/main/java/abilities/Charge.java
@@ -1,9 +1,16 @@
 package abilities;
 
+import cards.Card;
+
 public class Charge extends Ability {
     private boolean exhausted = false;
 
     public Charge() {
 
+    }
+
+    @Override
+    public boolean executeAbility(Card self, Card target) {
+        return true;
     }
 }
--- java_backend/src/main/java/abilities/Mechanic.java
+++ java_backend/src/main/java/abilities/Mechanic.java
@@ -15,4 +15,4 @@ public abstract class Mechanic {
     ;
 }
 
-}
+
