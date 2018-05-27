3533ed3db7d41615908567aeed59836cb5bd18da
Lambert Brend <brend.lambert@student.howest.be>
2018-05-27 02:33:25 +0200

    ColorFormats cleaned

--- java_backend/src/main/java/formatters/ColorFormats.java
+++ java_backend/src/main/java/formatters/ColorFormats.java
@@ -1,8 +1,9 @@
 package formatters;
 
-public class ColorFormats {
+public final class ColorFormats {
     // \033 means "format the next" number after [ means what to format and m is the end of the format.
-    // look at following link for more info -> TODO: http://www.santhoshreddymandadi.com/java/coloring-java-output-on-console.html
+    // look at following link for more info
+    // -> TODO: http://www.santhoshreddymandadi.com/java/coloring-java-output-on-console.html
     private static final String RESET = "\033[0m";
     private static final String RED = "\033[31m";
     private static final String GREEN = "\033[32m";
@@ -12,8 +13,10 @@ public class ColorFormats {
     private static final String CYAN = "\033[36m";
     private static final String GREY = "\033[37m";
 
+    private ColorFormats() { }
+
     public static String red(String str) {
-        return  RED + str + RESET;
+        return RED + str + RESET;
     }
 
     public static String green(String str) {
